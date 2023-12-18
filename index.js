ddeff76d82325181241cd64bf079e0bf534e85ee
victor <victor.odhi@gmail.com>
2023-12-18 23:11:04 +0300

    completed test

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solution here
+function findMatching(drivers, string) {
+    let matches = drivers.filter(name => name.toUpperCase() == string.toUpperCase());
+    return matches;
+}
+ 
+function fuzzyMatch(drivers, string) {
+     let matches = drivers.filter(name => name.charAt(0) == string.charAt(0));
+     return matches;
+}
+ 
+function matchName(drivers, string) {
+     let matches = drivers.filter(name1 => name1.name.toUpperCase() == string.toUpperCase());
+     return matches;
+}
